53498175de03009443204b063261521460c11d5b
amani-neo <neoxkenga@gmail.com>
2026-09-17 23:04:43 +0300

    Add files via upload

---
+++ error.c
@@ -0,0 +1,24 @@
+// varibles and Data Types 
+
+#include<stdio.h>
+
+int main(){
+	
+	//declare and initialize variables
+	char grade = 'A'; //%c
+	char name[15]= " Amani"; //string, %s
+	int age = 19; //%d
+	float pi = 3.142; //%f
+	double budget = 100024; //%lf
+	
+	printf("The grade is %c \n", grade);
+	printf("My name is %s \n", name);
+	printf("I am %d years old \n",age);
+	printf("The value of pi =%.4f \n", pi);
+	printf("My semester budget is %.2lf \n",budget);
+	
+	 return 0;
+	 
+}	 
+
+	  

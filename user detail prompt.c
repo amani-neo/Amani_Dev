71990accf8acfc0f39c4f19435ae75a66c87e94b
amani-neo <neoxkenga@gmail.com>
2026-09-22 00:28:07 +0300

    Add files via upload

---
+++ user detail prompt.c
@@ -0,0 +1,25 @@
+//declare variable
+ 
+# include<stdio.h>
+
+int main(int argc, char** argv)
+{
+	
+ float height =169;
+ double bankbalance = 24000;
+ char phonenumber[20] = "0769000363";
+ 
+ printf("Enter the value of height:");
+ scanf("%f",&height);
+ 
+ printf("Enter the bankbalance:");
+ scanf("%lf",&bankbalance);
+ 
+ printf("Enter the phonenumber:");
+ scanf("%s",&phonenumber);
+ 
+ return 0;
+ 
+}
+ 
+ 
